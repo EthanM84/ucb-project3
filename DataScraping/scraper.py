0b6f36b26806e0e7eb2b1e09ca7d512be19cbc44
Mohamed Abdelsalam <mohamedabdelsalam@MacBook-Pro.local>
2023-08-07 17:35:49 -0700

    data scraping rev 1

---
+++ DataScraping/scraper.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+def scrape():
+    url = 'https://www.bls.gov/oes/current/oes_nat.htm'
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        soup = bs(response.content, 'html.parser')
+        data_table = soup.find('table', class_='regular')
+
+        # Get the column headers
+        headers = data_table.find('thead').find_all('th')
+        column_headers = [header.text.strip() for header in headers]
+
+        # Initialize the data list to store the scraped data
+        scraped_data = []
+
+        # Get the rows of the table
+        rows = data_table.find('tbody').find_all('tr')
+        for row in rows:
+            columns = row.find_all('td')
+            row_data = {
+                column_headers[0]: columns[0].text.strip(),
+                column_headers[1]: columns[1].text.strip(),
+                column_headers[2]: columns[2].text.strip(),
+                column_headers[3]: columns[3].text.strip(),
+                column_headers[4]: columns[4].text.strip(),
+                column_headers[5]: columns[5].text.strip()
+            }
+            scraped_data.append(row_data)
+
+        return scraped_data
+    else:
+        print("Error fetching data:", response.status_code)
+        return []
+
+# Test the scraper
+if __name__ == "__main__":
+    data = scrape()
+    print(data)
--- DataScraping/static/script.js
+++ DataScraping/static/script.js
@@ -1,42 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Function to fetch the scraped data from the Flask backend
-    async function fetchData() {
-        try {
-            const response = await fetch('/get_data');
-            const data = await response.json();
-            return data.data;
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            return 'Error: Failed to fetch data.';
-        }
+async function updateScrapedData() {
+    const scrapedDataElement = document.querySelector('#scraped-data tbody');
+    scrapedDataElement.innerHTML = ''; // Clear existing rows from the table body
+
+    const scrapedData = await fetchData();
+    scrapedData.forEach((dataRow) => {
+        const row = document.createElement('tr');
+        Object.values(dataRow).forEach((value) => {
+            const cell = document.createElement('td');
+            cell.textContent = value;
+            row.appendChild(cell);
+        });
+        scrapedDataElement.appendChild(row);
+    });
+}
+
+async function fetchData() {
+    try {
+        const response = await fetch('/get_data');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
     }
+}
 
-    // Function to update the scraped data on the page
-    async function updateScrapedData() {
-        const scrapedDataElement = document.getElementById('scraped-data');
-        const scrapedData = await fetchData();
-        scrapedDataElement.textContent = scrapedData;
-    }
-
-    // Call the function to update the scraped data on page load
-    updateScrapedData();
-
-    // Toggle the dropdown menu
-    function toggleDropdown() {
-        const dropdownMenu = document.getElementById('dropdown-menu');
-        dropdownMenu.classList.toggle('show');
-    }
-
-    // Close the dropdown menu when clicking outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropdown button')) {
-            const dropdowns = document.getElementsByClassName('dropdown-content');
-            for (let i = 0; i < dropdowns.length; i++) {
-                const openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    };
-});
+// Initial update when the page loads
+updateScrapedData();
