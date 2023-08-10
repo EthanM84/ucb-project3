c51b19746e037b3ce1103c3aaad98f815a6a1cee
Ronda Hinz (PC) <130201073+RondaJoy@users.noreply.github.com>
2023-08-09 19:46:17 -0700

    Title Update

--- project3_redirect.js
+++ project3_redirect.js
@@ -186,7 +186,7 @@
             chart: {map: topology
                    },
 
-            title: {text: 'Highcharts Maps basic demo'},
+            title: {text: 'Jobs Lost to Automation: Impact by State'},
             subtitle: {text: 'Source map: <a href="http://code.highcharts.com/mapdata/countries/us/us-all.topo.json">United States of America</a>'},
 
             mapNavigation: {enabled: true, buttonOptions: {verticalAlign: 'bottom'}
